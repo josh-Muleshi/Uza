a9fc32df0b65244a5669a29512c2124006a6fbda
joshmuleshi <jmuleshi2@gmail.com>
2023-08-03 14:11:32 +0200

    add a splash screen and icon

--- app/src/main/java/cd/wayupdotdev/uza/MainActivity.kt
+++ app/src/main/java/cd/wayupdotdev/uza/MainActivity.kt
@@ -3,30 +3,44 @@ package cd.wayupdotdev.uza
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import cd.wayupdotdev.uza.ui.theme.UzaTheme
+import cd.wayupdotdev.uza.ui.viewModel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
+
         setContent {
             UzaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text(text = "Fist change", style = MaterialTheme.typography.bodySmall)
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(text = "Main Activity", style = MaterialTheme.typography.bodySmall)
+                    }
                 }
             }
         }
     }
+
 }
 
 @Preview
