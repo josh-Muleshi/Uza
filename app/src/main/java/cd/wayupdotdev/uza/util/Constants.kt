d065ca2078adf216bd27df183c8df0bb82c7b43c
joshmuleshi <jmuleshi2@gmail.com>
2023-08-11 02:48:42 +0200

    google sign in token

--- app/src/main/java/cd/wayupdotdev/uza/util/Constants.kt
+++ app/src/main/java/cd/wayupdotdev/uza/util/Constants.kt
@@ -1,5 +1,5 @@
 package cd.wayupdotdev.uza.util
 
 object Constants {
-    const val Token = "784126689037-4sdj0jslro4dtgim9vtu402jubfcvd7n.apps.googleusercontent.com"
+    const val Token = "980583905737-63v6dhqo7pndrt58upcpk1vs892ecavo.apps.googleusercontent.com"
 }
